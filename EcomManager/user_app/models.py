1c9244ae225dfa8e88d67c879a2beab87f8eb09c
Seena U <seena.u@seqato.com>
2024-07-15 20:18:19 +0530

    Issue fixed for creating superuser

--- EcomManager/user_app/models.py
+++ EcomManager/user_app/models.py
@@ -1,5 +1,26 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+
+class UserManager(BaseUserManager):
+    """
+    Class for customizing the User Model objects manager class
+    """
+
+    def create_superuser(self, email, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+        other_fields.setdefault('role', 'admin')  # Set the role to 'admin'
+        
+        return self.create_user(email, password, **other_fields)
+
+    def create_user(self, email, password=None, **other_fields):
+        user = self.model(email=email, **other_fields)
+        if password:
+            user.set_password(password)
+        user.save()
+        return user
+
 
 class CustomUser(AbstractUser):
     role_choices = (
@@ -14,6 +35,8 @@ class CustomUser(AbstractUser):
     username = models.CharField(max_length=120, blank=True, null=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     # Customize how the user is displayed in the admin panel
     def __str__(self):
